29b02bb814eae00128e23f723d2a9c8fad7caf5e
ujeans <dbwlsxkal95@gmail.com>
2024-11-03 20:59:42 +0900

    feat(feature/#7): Reward 컴포넌트 3초 후에 자동 닫힘 후 /페이지로 이동하기 구현

--- src/components/common/Reward.tsx
+++ src/components/common/Reward.tsx
@@ -1,18 +1,22 @@
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 import RewardText from './RewardText'
 
 import { RewardProps } from '@/types/Reward'
 
 export default function Reward(props: RewardProps) {
-  const { yaho, rewardContent, onClose } = props
+  const router = useRouter()
   const [showReward, setShowReward] = useState(true)
+
+  const { yaho, rewardContent, onClose } = props
 
   useEffect(() => {
     const timer: NodeJS.Timeout = setTimeout(() => {
       setShowReward(true)
       onClose()
+      router.push('/')
     }, 3000)
     return () => clearTimeout(timer)
   }, [onClose])
