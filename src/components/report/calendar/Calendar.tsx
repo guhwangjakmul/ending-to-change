2e51b7d06aa9293e201106f031cb749e65116a20
ujeans <dbwlsxkal95@gmail.com>
2024-11-08 14:27:47 +0900

    design(feature/#30): 날짜 클릭시 조건에 따라 다른 스타일링 적용

--- src/components/report/calendar/Calendar.tsx
+++ src/components/report/calendar/Calendar.tsx
@@ -54,8 +54,19 @@ export default function CustomCalendar(props: CustomCalendarProps) {
         showNeighboringMonth={false}
         minDetail="year"
         tileClassName={({ date }) => {
-          // 특정 날짜에 민트색 동그라미 추가
-          return date.getTime() === specialDate.getTime() ? 'highlight-circle' : ''
+          if (moment(date).isSame(today, 'day') && moment(date).isSame(selectedDate, 'day')) {
+            return 'today-selected' // 오늘 날짜가 선택된 경우 주황색 배경 유지
+          }
+          if (moment(date).isSame(today, 'day') && !moment(date).isSame(selectedDate, 'day')) {
+            return 'today' // 오늘 날짜가 선택되지 않은 경우 주황색 폰트
+          }
+          if (selectedDate && moment(date).isSame(selectedDate, 'day')) {
+            return 'clicked-date' // 선택된 날짜가 오늘이 아닌 경우 브라운 박스
+          }
+          if (date.getTime() === specialDate.getTime()) {
+            return 'highlight-circle' // 특정 날짜(걸은 날이 있을 때)에 민트색 동그라미 추가
+          }
+          return ''
         }}
       />
     </div>
