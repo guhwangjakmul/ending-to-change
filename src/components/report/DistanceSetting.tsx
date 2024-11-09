61714c117fa3579d63168ee84cf54fbe80c5471b
ujeans <dbwlsxkal95@gmail.com>
2024-11-09 23:16:48 +0900

    feat(feature/#35): 거리 설정에서 input을 이용한 RangeSlider 구현

--- src/components/report/DistanceSetting.tsx
+++ src/components/report/DistanceSetting.tsx
@@ -1,3 +1,11 @@
+import { useEffect, useState } from 'react'
+
+import Modal from '../common/Modal'
+import Button from '../common/Button'
+import RangeSlider from './RangeSlider'
+
+import useModal from '@/app/hook/useModal'
+
 interface DistanceSettingProps {
   goalKm: number
   setGoalKm: (value: number) => void
@@ -5,17 +13,44 @@ interface DistanceSettingProps {
 
 export default function DistanceSetting(props: DistanceSettingProps) {
   const { goalKm, setGoalKm } = props
+  const [isOpen, openModal, closeModal, portalElement] = useModal()
+  const [temporaryGoal, setTemporaryGoal] = useState(goalKm)
 
-  const handleGoalChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setGoalKm(Number(e.target.value))
+  useEffect(() => {
+    if (isOpen) {
+      setTemporaryGoal(goalKm)
+    }
+  }, [isOpen, goalKm])
+
+  const handleGoalChange = () => {
+    setGoalKm(temporaryGoal)
+    closeModal()
   }
 
   return (
     <div className="flex justify-end mb-[10px] pr-2">
-      <span className="font-gosindinaru-m text-xs text-light-gray border-b border-light-gray">
+      <span
+        onClick={openModal}
+        className="font-gosindinaru-m text-xs text-light-gray border-b border-light-gray cursor-pointer"
+      >
         거리 설정
       </span>
-      <input type="number" value={goalKm} onChange={handleGoalChange} />
+      {portalElement && isOpen && (
+        <Modal height={273} onClick={closeModal}>
+          <span className="mb-[19px]">하루 목표를 설정해주세요!</span>
+          <div className="bg-white w-[100px] h-[30px] rounded-[10px] mb-[30px] flex justify-center items-center font-sindinaru-m text-dark-brown text-[14px]">
+            {temporaryGoal}km
+          </div>
+          <RangeSlider
+            currentProgress={temporaryGoal}
+            onChange={setTemporaryGoal}
+            isOpen={isOpen}
+          />
+          <Button width={100} height={40} fontSize={12} onClick={handleGoalChange}>
+            결정
+          </Button>
+        </Modal>
+      )}
     </div>
   )
 }
