8bb59a88f892dbfa5894d36f3e30a40a8d295604
ujeans <dbwlsxkal95@gmail.com>
2024-11-09 17:58:25 +0900

    feat(feature/#35): /페이지에서 목표 설정 거리, 오늘 걸은 거리를 각 자식 컴포넌트에 전달 후 ProgressBar에 오늘 걸은 거리, 목표 거리 전달

--- src/components/report/DistanceSetting.tsx
+++ src/components/report/DistanceSetting.tsx
@@ -1,9 +1,21 @@
-export default function DistanceSetting() {
+interface DistanceSettingProps {
+  goalKm: number
+  setGoalKm: (value: number) => void
+}
+
+export default function DistanceSetting(props: DistanceSettingProps) {
+  const { goalKm, setGoalKm } = props
+
+  const handleGoalChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGoalKm(Number(e.target.value))
+  }
+
   return (
     <div className="flex justify-end mb-[10px] pr-2">
       <span className="font-gosindinaru-m text-xs text-light-gray border-b border-light-gray">
         거리 설정
       </span>
+      <input type="number" value={goalKm} onChange={handleGoalChange} />
     </div>
   )
 }
--- src/components/report/TodayEcoStats.tsx
+++ src/components/report/TodayEcoStats.tsx
@@ -1,28 +1,22 @@
-<<<<<<< HEAD
 import ProgressBar from '../common/ProgressBar'
-=======
->>>>>>> develop
+
 import Wrapper from './ReportWrapper'
 
 interface TodayEcoStatsProps {
   selectedDate: Date
+  goalKm: number
+  todaysDistance: number
 }
 
 export default function TodayEcoStats(props: TodayEcoStatsProps) {
-<<<<<<< HEAD
-  const goalKm = 5
-  const { selectedDate } = props
+  const { selectedDate, goalKm, todaysDistance } = props
 
   return (
     <Wrapper>
-      <ProgressBar labelType="Km" currentProgress={2.4} maxKm={goalKm} />
+      <ProgressBar labelType="Km" currentProgress={todaysDistance} maxKm={goalKm} />
       <span className="inline-block text-center text-[15px] mt-[30px]">
         탄소배출량을 <span className="text-sky-blue">7.2kg CO₂e</span> 줄였어요!
       </span>
     </Wrapper>
   )
-=======
-  const { selectedDate } = props
-  return <Wrapper> {selectedDate.toLocaleDateString('ko-KR')}</Wrapper>
->>>>>>> develop
 }
