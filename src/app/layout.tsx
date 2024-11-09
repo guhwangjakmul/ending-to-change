a231ca6e5616c5696b95ea6e82a603ceaed6cf3a
ujeans <dbwlsxkal95@gmail.com>
2024-11-10 02:38:19 +0900

    design(feature/#21): 이미지 밀리는 현상 이슈 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body suppressHydrationWarning={true} className="flex items-center justify-center">
-        <div className="max-w-[390px] w-screen h-screen bg-light-beige relative">
+        <div className="max-w-[390px] w-screen h-screen bg-light-beige relative  flex flex-col">
           <HeaderLayout />
           {children}
           <div id="portal"></div>
