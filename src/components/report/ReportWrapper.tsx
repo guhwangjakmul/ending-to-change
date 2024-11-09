cd926392aba750085292e3354c39cb9494a46fe3
ujeans <dbwlsxkal95@gmail.com>
2024-11-09 17:38:56 +0900

    design(feature/#35): ReportWrapper Props에 paddingX 추가 및 스타일 추가

--- src/components/report/ReportWrapper.tsx
+++ src/components/report/ReportWrapper.tsx
@@ -1,7 +1,6 @@
 interface ReportWrapperProps {
   children: React.ReactNode
   height?: number
-<<<<<<< HEAD
   paddingX?: number
 }
 
@@ -13,15 +12,6 @@ export default function ReportWrapper(props: ReportWrapperProps) {
       className={`w-full mb-[39px] py-[30px]  bg-beige rounded-[15px] flex flex-col items-center`}
       style={{ height: `${height}px`, padding: `${paddingX}px` }}
     >
-=======
-}
-
-export default function ReportWrapper(props: ReportWrapperProps) {
-  const { children, height = 142 } = props
-
-  return (
-    <div className={`w-full mb-[39px] bg-beige rounded-[15px]`} style={{ height: `${height}px` }}>
->>>>>>> develop
       {children}
     </div>
   )
