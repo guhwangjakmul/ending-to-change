5aa0c26bfc4458f5e19a8a3cac0a8787689ad089
Lami <Halam.Lee.Lami@gmail.com>
2024-11-09 21:31:09 +0900

    modify(feature/#33): url 경로에 따라 레이아웃 배경색 지정하지 않도록 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import '@/styles/globals.css'
 import HeaderLayout from '@/components/layout/HeaderLayout'
+import BackgroundWrapper from '@/components/layout/BackgroundWrapper'
 
 export const metadata: Metadata = {
   title: '구해줘요 동물의숲',
@@ -15,10 +16,10 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body suppressHydrationWarning={true} className="flex items-center justify-center">
-        <div className="max-w-[390px] w-screen h-screen bg-light-beige">
+        <BackgroundWrapper>
           <HeaderLayout />
           {children}
-        </div>
+        </BackgroundWrapper>
       </body>
     </html>
   )
---
+++ src/components/layout/BackgroundWrapper.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+
+interface BackgroundWrapperProps {
+  children: React.ReactNode
+}
+
+export default function BackgroundWrapper(props: BackgroundWrapperProps) {
+  const { children } = props
+
+  const pathname = usePathname()
+  const whiteBackground = ['/', '/walk', '/ranking', '/report']
+
+  return (
+    <div
+      className={`max-w-[390px] w-screen h-screen ${
+        whiteBackground.includes(pathname) ? '' : 'bg-light-beige'
+      }`}
+    >
+      {children}
+    </div>
+  )
+}
