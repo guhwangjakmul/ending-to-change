70a77802e10c18a8b051351303849f74b19c6f1a
ujeans <dbwlsxkal95@gmail.com>
2024-11-09 17:39:35 +0900

    design(feature/#35): props PaddingX 추가

--- src/components/report/WeeklyEcoChart.tsx
+++ src/components/report/WeeklyEcoChart.tsx
@@ -8,11 +8,7 @@ export default function WeeklyEcoChart(props: WeeklyEcoChartProps) {
   const { weekRange } = props
 
   return (
-<<<<<<< HEAD
     <Wrapper height={280} paddingX={70}>
-=======
-    <Wrapper height={280}>
->>>>>>> develop
       {weekRange && (
         <div>
           <div>{`일주일 범위: ${weekRange.start} ~ ${weekRange.end}`}</div>
