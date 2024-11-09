89d372c772a1359a98c2a86b1f7d56eb0c4518d5
ujeans <dbwlsxkal95@gmail.com>
2024-11-09 20:19:31 +0900

    feat(feature/#35): 걸은 거리에 따른 탄소 배출량 계산

--- src/components/report/TodayEcoStats.tsx
+++ src/components/report/TodayEcoStats.tsx
@@ -11,11 +11,14 @@ interface TodayEcoStatsProps {
 export default function TodayEcoStats(props: TodayEcoStatsProps) {
   const { selectedDate, goalKm, todaysDistance } = props
 
+  // 탄소 배출량 계산
+  const carbon = ((todaysDistance / 16.04) * 2.097).toFixed(1)
+
   return (
     <Wrapper>
       <ProgressBar labelType="Km" currentProgress={todaysDistance} maxKm={goalKm} />
       <span className="inline-block text-center text-[15px] mt-[30px]">
-        탄소배출량을 <span className="text-sky-blue">7.2kg CO₂e</span> 줄였어요!
+        탄소배출량을 <span className="text-sky-blue">{carbon}kg CO₂e</span> 줄였어요!
       </span>
     </Wrapper>
   )
