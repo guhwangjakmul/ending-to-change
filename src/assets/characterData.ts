a5d9a89fbdb29105a1b845385ce815a1b7f54c48
leejihee <jihee6986@naver.com>
2024-11-19 14:40:11 +0900

    data(feature/#22): 주민데이터 수정

--- src/assets/characterData.ts
+++ src/assets/characterData.ts
@@ -57,6 +57,11 @@ const characterGroup = {
       '/image/character/energy_2.svg',
       '/image/character/energy_3.svg',
     ],
+    background: [
+      '/image/background/energy_1.svg',
+      '/image/background/energy_2.svg',
+      '/image/background/energy_3.svg',
+    ],
     dialogues: [
       '흐음… 저 반짝이는 별들을 관찰하려 했건만, 도시 불빛이 너무도 강렬하군요…',
       '어라? 오늘은 저 먼 별들이 희미하게 모습을 드러내기 시작했군요. 아주 흥미로운 변화입니다!',
@@ -83,16 +88,16 @@ const characterGroup = {
       '/image/background/air_2.svg',
       '/image/background/air_3.svg',
     ],
-    levelupText: [
-      '바닷속 쓰레기가 점점 줄고있어~!',
-      '물고기가 돌아오고 있어~!',
-      '바다가 깨끗해졌어~!',
-    ],
     dialogues: [
       '요새 공기가 너무 탁해져서 숨 쉬기 힘들어구리… 나갈 때는 마스크가 필요해',
       '오늘은 공기가 조금 더 맑아진 느낌이야구리! 숨 쉬는 게 살짝 편해진 것 같아서 다행이야구리.',
       '오오, 이제 공기가 정말 상쾌하네구리! 이렇게나 맑은 공기라니, 숨 쉴 때마다 몸이 가뿐해져구리!',
       '고마워구리! 덕분에 이렇게 깨끗한 공기를 다시 마시게 됐어구리. 이 은혜는 평생 잊지 않을게구리!',
+    ],
+    levelupText: [
+      '바닷속 쓰레기가 점점 줄고있어~!',
+      '물고기가 돌아오고 있어~!',
+      '바다가 깨끗해졌어~!',
     ],
     koreanName: '너굴',
     nameBg: '#DF852D',
@@ -104,16 +109,21 @@ const characterGroup = {
       '/image/character/soil_2.svg',
       '/image/character/soil_3.svg',
     ],
-    levelupText: [
-      '바닷속 쓰레기가 점점 줄고있어~!',
-      '물고기가 돌아오고 있어~!',
-      '바다가 깨끗해졌어~!',
+    background: [
+      '/image/background/soil_1.svg',
+      '/image/background/soil_2.svg',
+      '/image/background/soil_3.svg',
     ],
     dialogues: [
       '아이고… 땅이 엉망이 됐네. 흙이 이렇게 나빠져서는 무가 자랄 틈이 없구만…',
       '에구, 이번엔 무가 썩었네... 흙이 아직 완전히 회복되지 않은가 보네. 제대로 된 무를 언제쯤 볼 수 있으려나…',
       '제 흙이 제대로 좋아졌네! 무가 쑥쑥 자라서 밭이 파릇파릇하구만~ 이러면 훌륭한 무 수확을 기대해도 되겠는걸?',
       '정말 고마워~ 덕분에 이 무파니가 다시 무를 팔 수 있게됐어! 앞으로도 잘 부탁해~',
+    ],
+    levelupText: [
+      '바닷속 쓰레기가 점점 줄고있어~!',
+      '물고기가 돌아오고 있어~!',
+      '바다가 깨끗해졌어~!',
     ],
     koreanName: '무파니',
     nameBg: '#FF9A3E',
@@ -125,16 +135,21 @@ const characterGroup = {
       '/image/character/recycle_2.svg',
       '/image/character/recycle_3.svg',
     ],
-    levelupText: [
-      '바닷속 쓰레기가 점점 줄고있어~!',
-      '물고기가 돌아오고 있어~!',
-      '바다가 깨끗해졌어~!',
+    background: [
+      '/image/background/recycle_1.svg',
+      '/image/background/recycle_2.svg',
+      '/image/background/recycle_3.svg',
     ],
     dialogues: [
       '쓰레기들이 아무렇게나 널브러져 있으이, 여가 시장통인지 쓰레기장인지 헷갈린다니까예…',
       '좀 깨끗해진 거 같지예? 길도 한결 말끔해진 기분이다 아이가. 누가 고생을 하고 있는 기라예~',
       '이제 분리수거통까지 딱딱 자리 잡았고 거리도 아주 깨끗해졌데이! 여가 다니기 참 좋네예!',
       '참말로 고맙심더! 덕분에 거리가 이렇게나 말끔해지고 살기 좋아졌심더.',
+    ],
+    levelupText: [
+      '바닷속 쓰레기가 점점 줄고있어~!',
+      '물고기가 돌아오고 있어~!',
+      '바다가 깨끗해졌어~!',
     ],
     koreanName: '고순이',
     nameBg: '#354875',
