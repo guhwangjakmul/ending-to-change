769f70b1d8316e9946d88f863877192d499e770f
ryukyunghye <ryukyung123@naver.com>
2024-11-22 02:30:59 +0900

    feat(feat/#36): 회원정보 수정 페이지 닉네임 수정 및 프로필 이미지 사이즈 수정

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,18 +1,37 @@
+'use client'
 import CategoryField from '@/components/common/CategoryField'
 import UserProfile from '@/components/user/UserProfile'
+import { getUserId } from '@/utils/user/auth'
+import { getUserInfo } from '@/utils/user/user'
+import { useEffect, useState } from 'react'
 
 export default function MyPage() {
-  const userInfo = {
-    nickName: '가오리영감탱구리임다',
-    categoryCount: 1,
-    profile: '/image/default_profile.svg',
-  }
+  const [user, setUser] = useState({ user_id: '', nickname: '', avatar_url: '' })
+  const categoryCount = 3
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      const userId = await getUserId()
+      if (!userId) return console.error('User ID not found')
+
+      const userInfo = await getUserInfo(userId)
+      if (userInfo) {
+        setUser({
+          user_id: userId,
+          nickname: userInfo[0].nickname,
+          avatar_url: userInfo[0].avatar_url,
+        })
+      }
+    }
+    fetchUserInfo()
+  }, [])
+
   return (
     <main className="w-full h-screen flex items-center flex-col p-[35px]">
       <div className="w-full flex items-center gap-[15px]">
-        <UserProfile size={50} isEditable={false} />
+        <UserProfile size={50} isEditable={false} imgSrc={user.avatar_url} />
         <span className="font-gothic-m text-brown text-[12px] whitespace-nowrap">
-          {userInfo.nickName}님, 현재 뱃지 {userInfo.categoryCount}개를 획득했어요
+          {user.nickname || '구해줘요 동물의 숲'}님<br /> 현재 뱃지 {categoryCount}개를 획득했어요
         </span>
       </div>
       <CategoryField isClickable={false} />
