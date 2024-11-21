06eb4573fd09781c5b0daefc06d61008fb26a954
ryukyunghye <ryukyung123@naver.com>
2024-11-22 02:25:00 +0900

    feat(feat/#36): 미들웨어 추가(추후에 수정 예정)

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next()
+
+  console.log('middleware pass')
+  return response
+}
+
+export const config = {
+  matcher: ['/', '/auth'],
+}
