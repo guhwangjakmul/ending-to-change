f1115e0888832b9003ba08559e3ad71e0c05727e
Lami <Halam.Lee.Lami@gmail.com>
2024-11-24 03:35:24 +0900

    modify(feature/#49): SpotButton, CategoryField 컴포넌트 수정

--- src/types/CategoryField.ts
+++ src/types/CategoryField.ts
@@ -1,10 +1,16 @@
-export type CategoryName = 'air' | 'energy' | 'recycle' | 'soil' | 'warming' | 'water'
+export type CategoryName =
+  | '대기오염'
+  | '에너지 절약'
+  | '분리수거'
+  | '토양오염'
+  | '지구온난화'
+  | '수질오염'
 type CategoryStatus = 'default' | 'selected' | 'completed'
 type isClickable = boolean
 
 export interface Category {
   name: CategoryName
-  status: CategoryStatus
+  status?: CategoryStatus
 }
 
 export interface SpotButtonProps extends Category {
@@ -13,5 +19,7 @@ export interface SpotButtonProps extends Category {
 }
 
 export interface CategoryFieldProps {
+  categoryList: Category[]
   isClickable?: isClickable
+  onClick?: () => void
 }
