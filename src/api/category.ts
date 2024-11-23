8cabb551a21014d7efb42fae274c47afe8c5d0a3
Lami <Halam.Lee.Lami@gmail.com>
2024-11-24 03:34:34 +0900

    feat(feature/#49): 수파베이스 이용한 카테고리 api 작성

---
+++ src/api/category.ts
@@ -0,0 +1,58 @@
+import { createSupabaseBrowserClient } from '@/utils/client/supabase'
+
+const supabase = createSupabaseBrowserClient()
+
+/**
+ *
+ * @param id 유저의 uuid
+ * @returns category_progress 테이블에서 해당 유저의 데이터 리스트
+ */
+export const getCategoryProgress = async (id: string) => {
+  const { data, error } = await supabase.from('category_progress').select('*').eq('user_id', id)
+  if (error) {
+    console.error('데이터 패칭 실패', error)
+    return []
+  }
+  return data
+}
+
+/**
+ * 전체 카테고리 데이터 리스트를 반환하는 함수
+ * @returns 전체 카테고리 데이터 리스트
+ */
+export const getAllCategoryList = async () => {
+  const { data, error } = await supabase.from('category').select('*')
+  if (error) {
+    console.error('데이터 패칭 실패', error)
+    return []
+  }
+  return data
+}
+
+/**
+ * 카테고리 아이디에 해당하는 카테고리명을 반환하는 함수
+ * @param idList 카테고리 id 배열
+ */
+export const getCategoryList = async (idList: number[]) => {
+  const { data, error } = await supabase.from('category').select('*').in('id', idList)
+  if (error) {
+    console.error('데이터 패칭 실패', error)
+    return []
+  }
+  return data
+}
+
+/**
+ * 유저가 성공한 카테고리 리스트를 반환하는 함수
+ * @param id 유저 uuid
+ * @returns 성공한 카테고리 리스트
+ */
+export const getCompletedCategoryList = async (id: string) => {
+  const userCategoryList = getCategoryProgress(id)
+  const completedCategoryList = (await userCategoryList).filter(data => data.is_completed)
+  const completedCategoryIdList = completedCategoryList.map(data => data.category_id)
+
+  const categoryList = await getCategoryList(completedCategoryIdList)
+
+  return categoryList
+}
