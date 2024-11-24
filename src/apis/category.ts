e7a1dbb387a6d609c57634962acad4555dc0d8a6
ujeans <dbwlsxkal95@gmail.com>
2024-11-24 15:09:32 +0900

    refactor(feature/#44): 공통되는 supabase 외부로 밖으로 빼기

--- src/apis/category.ts
+++ src/apis/category.ts
@@ -1,33 +1,33 @@
-"use client";
+'use client'
 
-import { createSupabaseBrowserClient } from "@/utils/client/supabase";
-import { Database } from "@/types/supabase";
+import { createSupabaseBrowserClient } from '@/utils/client/supabase'
+import { Database } from '@/types/supabase'
 
 type CategoryInfo = Database['public']['Tables']['category_progress']['Row']
 
-// category_progress 가져오기
-export const getCategoryProgress = async (user_id: string): Promise<CategoryInfo | null>  => {
-  const supabase = createSupabaseBrowserClient();
+const supabase = createSupabaseBrowserClient()
 
+// category_progress 가져오기
+export const getCategoryProgress = async (user_id: string): Promise<CategoryInfo | null> => {
   const { data } = await supabase
-    .from("category_progress")
-    .select("*")
-    .eq("user_id", user_id)
+    .from('category_progress')
+    .select('*')
+    .eq('user_id', user_id)
     .limit(1)
     .single()
-    
+
   return data
 }
 
 // category_progress의 progress 업데이트
-export const updateProgress  = async (user_id: string, points: number) => {
-  const supabase = createSupabaseBrowserClient();
+export const updateProgress = async (user_id: string, points: number) => {
+  const supabase = createSupabaseBrowserClient()
 
   const currentProgress = await getCategoryProgress(user_id)
-  const currentPoints = currentProgress?.progress ?? 0; 
-  
+  const currentPoints = currentProgress?.progress ?? 0
+
   await supabase
-    .from("category_progress")
-    .update({ progress: (currentPoints + points)})
-    .eq("user_id", user_id)
+    .from('category_progress')
+    .update({ progress: currentPoints + points })
+    .eq('user_id', user_id)
 }
--- src/apis/quiz.ts
+++ src/apis/quiz.ts
@@ -1,74 +1,67 @@
-"use client";
+'use client'
 
+import { createSupabaseBrowserClient } from '@/utils/client/supabase'
+import { getUserInfo, updateUser } from '@/utils/user/user'
 
-import { createSupabaseBrowserClient } from "@/utils/client/supabase";
-import { getUserInfo, updateUser } from "@/utils/user/user";
-
+const supabase = createSupabaseBrowserClient()
 
 // 특정 사용자가 풀지 않은 퀴즈 가져오기
 export const getUnsolvedQuizzes = async (user_id: string, category_id: number) => {
-  const supabase = createSupabaseBrowserClient();
-
   const { data: quizData, error: quizError } = await supabase
-    .from("quiz")
-    .select("*")
-    .eq("category_id", category_id); 
+    .from('quiz')
+    .select('*')
+    .eq('category_id', category_id)
 
   if (quizError) {
-    console.error("Error fetching quiz data from Supabase:", quizError.message);
-    return [];
+    console.error('Error fetching quiz data from Supabase:', quizError.message)
+    return []
   }
 
   const { data: quizLogs, error: quizLogError } = await supabase
-    .from("quiz_log")
-    .select("*")
-    .eq("user_id", user_id); 
+    .from('quiz_log')
+    .select('*')
+    .eq('user_id', user_id)
 
   if (quizLogError) {
-    console.error("Error fetching quiz_log data from Supabase:", quizLogError.message);
-    return [];
+    console.error('Error fetching quiz_log data from Supabase:', quizLogError.message)
+    return []
   }
 
-  const solvedQuizIds = quizLogs?.map((quizLog) => quizLog.quiz_id) || [];
-  const unSolvedQuizzes = quizData?.filter((quiz) => !solvedQuizIds.includes(quiz.id)) || [];
+  const solvedQuizIds = quizLogs?.map(quizLog => quizLog.quiz_id) || []
+  const unSolvedQuizzes = quizData?.filter(quiz => !solvedQuizIds.includes(quiz.id)) || []
 
-  return unSolvedQuizzes;
-};
+  return unSolvedQuizzes
+}
 
 // 사용자 point 업데이트
 export const updateUserPoint = async (user_id: string, point: number) => {
   try {
-    const userInfo = await getUserInfo(user_id);
+    const userInfo = await getUserInfo(user_id)
 
     if (!userInfo || userInfo.length === 0) {
-      console.error("User not found.");
-      return;
+      console.error('User not found.')
+      return
     }
 
-    const currentPoint = userInfo[0].point ?? 0; 
-    const newPoint = currentPoint + point;
+    const currentPoint = userInfo[0].point ?? 0
+    const newPoint = currentPoint + point
 
     if (isNaN(newPoint)) {
-      throw new Error("New points cannot be NaN. Check input values.");
+      throw new Error('New points cannot be NaN. Check input values.')
     }
 
-    const isUpdated = await updateUser(user_id, "point", newPoint);
+    const isUpdated = await updateUser(user_id, 'point', newPoint)
 
     if (!isUpdated) {
-      console.error("Failed to update user points.");
-      return;
+      console.error('Failed to update user points.')
+      return
     }
-
   } catch (error) {
-    console.error("Error in updateUserPoint:", error);
+    console.error('Error in updateUserPoint:', error)
   }
 }
 
 // 퀴즈 로그 저장
 export const insertQuizLog = async (quiz_id: number, user_id: string) => {
-  const supabase = createSupabaseBrowserClient();
-
-  await supabase
-    .from("quiz_log")
-    .insert({quiz_id, user_id})
+  await supabase.from('quiz_log').insert({ quiz_id, user_id })
 }
