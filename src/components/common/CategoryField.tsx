4fd4ab3f3818196059614e78819d91eccd600c8d
Lami <Halam.Lee.Lami@gmail.com>
2024-11-26 00:12:06 +0900

    feat(feature/#49): 카테고리 선택 페이지 구현

--- src/components/common/CategoryField.tsx
+++ src/components/common/CategoryField.tsx
@@ -1,26 +1,51 @@
 'use client'
 import Image from 'next/image'
 import SpotButton from './SpotButton'
-import { CategoryFieldProps } from '@/types/CategoryField'
+import { CategoryFieldProps, CategoryName } from '@/types/CategoryField'
+import { useEffect, useState } from 'react'
 
 export default function CategoryField(props: CategoryFieldProps) {
-  const { categoryList, isClickable = false, onClick } = props
+  const { categoryList, isClickable = false, onClick, setSelectCategory } = props
 
-  const buttonClickHandler = () => {
+  const [useCategoryList, setUseCategoryList] = useState(categoryList)
+
+  const buttonClickHandler = (name: CategoryName) => {
+    if (setSelectCategory) {
+      setSelectCategory(name)
+    }
+
     if (!isClickable) return
-    if (onClick) return onClick
+    setUseCategoryList(prevList => {
+      const clickedCategory = prevList.find(category => category.name === name)
+
+      if (clickedCategory?.status === 'completed') return prevList
+
+      return prevList.map(category => {
+        if (category.name === name)
+          return {
+            ...category,
+            status: category.status === 'default' ? 'selected' : 'selected',
+          }
+        else if (category.status !== 'completed') return { ...category, status: 'default' }
+        return category
+      })
+    })
   }
+
+  useEffect(() => {
+    setUseCategoryList(categoryList)
+  }, [categoryList])
 
   return (
     <div className="w-[232px] h-[232px] relative m-auto">
-      {categoryList &&
-        categoryList.map(category => (
+      {useCategoryList &&
+        useCategoryList.map(category => (
           <SpotButton
             key={category.name}
             name={category.name}
             status={category.status}
             isClickable={isClickable}
-            onClick={buttonClickHandler}
+            onClick={() => buttonClickHandler(category.name)}
           />
         ))}
       <Image src="/image/earth.svg" alt="" width="233" height="233" />
