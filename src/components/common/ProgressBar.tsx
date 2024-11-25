aa27afc498aca93b38bba28182c5afee4b521397
ujeans <dbwlsxkal95@gmail.com>
2024-11-26 02:51:41 +0900

    design(feature/#57): 카테고리별 progressbar label 텍스트 색상 다르게 구현

--- src/components/common/ProgressBar.tsx
+++ src/components/common/ProgressBar.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import { ProgressBarProps, LabelType, ProgressColor, BackgroundColor } from '@/types/ProgressBar'
+import { useEffect, useState } from 'react'
 
 export default function ProgressBar(props: ProgressBarProps) {
   const { labelType = 'Lv', currentProgress, level, showLabel = true, maxKm } = props
@@ -8,9 +11,16 @@ export default function ProgressBar(props: ProgressBarProps) {
   // 진행 퍼센티지를 계산
   const progressPercentage = (currentProgress / max) * 100
 
+  const [category, setCategory] = useState<string | null>(null)
+
+  useEffect(() => {
+    const storedCategory = localStorage.getItem('category')
+    setCategory(storedCategory)
+  }, [])
+
   return (
     <div className="flex flex-col w-full space-y-2">
-      {showLabel && renderLabel(labelType, currentProgress, max, level)}
+      {showLabel && renderLabel(labelType, currentProgress, max, level, category)}
       <div className="relative w-full h-3 flex items-center space-x-2">
         <div className={`w-full h-3 ${getBackgroundColor(labelType)} rounded-full overflow-hidden`}>
           <div
@@ -51,12 +61,19 @@ const renderLabel = (
   currentProgress?: number,
   max?: number,
   level?: number,
+  category?: string | null,
 ) => {
   const fontSize = labelType === 'Lv' ? 'text-[12px]' : 'text-[14px]'
   const fontStyle = labelType === 'Lv' ? 'font-sindinaru-b' : 'font-gothic-b'
 
+  // 카테고리에 따라 텍스트 색상을 변경
+  const textColor =
+    category === '수질오염' || category === '에너지 절약' || category === '토양오염'
+      ? 'text-cream'
+      : 'text-brown'
+
   return (
-    <div className={`text-brown flex items-center justify-between ${fontSize} ${fontStyle}`}>
+    <div className={`flex items-center justify-between ${fontSize} ${fontStyle} ${textColor}`}>
       {/* 'Lv' 타입일 경우 레벨 정보 표시 */}
       {labelType === 'Lv' ? <span>Lv.{level}</span> : <span>오늘 걸은 거리</span>}
       <span>
